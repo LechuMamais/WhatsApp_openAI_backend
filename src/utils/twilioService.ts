4b78ccd351e1f7b2ccd303ea949c8dad15e9132d
LechuMamais <119885029+LechuMamais@users.noreply.github.com>
2024-10-29 20:34:43 +0100

    UPDATE: debugging twilioService

--- src/utils/twilioService.ts
+++ src/utils/twilioService.ts
@@ -5,6 +5,7 @@ dotenv.config();
 const client = twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
 
 export const sendWhatsappMessage = async (to: string, message: string): Promise<void> => {
+    console.log('to: ', to)
 
     await client.messages.create({
         body: message,
