ded89bf96b59dee1ffc2c9cca8ec0f980d6625d0
LechuMamais <119885029+LechuMamais@users.noreply.github.com>
2025-05-29 16:24:53 +0200

    FIXED: TS error importing file

--- src/routes/aiRoutes.ts
+++ src/routes/aiRoutes.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { getAIResponse } from '../controllers/aiController';
+import { getAIResponse } from '../controllers/aicontroller';
 
 const aiRouter = Router();
 
